ff91d52303f13de4ceb00d7d47bf7a4db33fccef
Hayate Kuronuma <hayate.kuronuma@gmail.com>
2021-08-20 08:12:06 +0000

    first-commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,57 @@
+class UsersController < ApplicationController
+  
+  def index
+  end
+  
+  def signup
+    @user = User.new
+  end
+  
+  def create
+     @user = User.new(params.require(:user).permit(:name, :email, :password, :password_confirmation), image_name: "default_icon.jpg")
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to ("/")
+    else
+      render "signup"
+    end
+  end 
+  
+  def show
+  end 
+  
+  def edit
+  end 
+  
+  def update
+  end 
+  
+  def destroy
+  end 
+  
+  def acount
+  end 
+  
+  def signin_form
+  end
+  
+  def signin
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      flash[:notice] = "ログインしました"
+      redirect_to ("/")
+    else
+      @error_message = "メールアドレスまたはパスワードが間違っています"
+      @email = params[:email]
+      @password = params[:password]
+      render "signin_form"
+    end
+  end
+  
+  def signout
+    session[:user_id] = nill
+    flash[:notice] = "ログアウトしました"
+    redirect_to ("/")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get '/', to: 'home#top'
+  
+  get 'users/acount', to: 'users#acount'
+  get 'users/signup', to: 'users#signup'
+  get 'users/signin', to: 'users#signin_form'
+  post 'users/signup', to: 'users#create'
+  post 'users/signin', to: 'users#signin'
+  post 'logout', to: 'users#signout'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
